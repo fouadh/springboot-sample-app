232cafb38a4361bc95ec4e4bbf500ac4f8fb54b5
Fouad <1621862+fouadh@users.noreply.github.com>
2020-07-21 14:19:55 +0200

    replace world with sunshine and bump the version

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -8,7 +8,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello Sunshine!";
     }
 
     public static void main(String[] args) {
